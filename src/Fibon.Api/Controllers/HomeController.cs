df1e02677e4f44c9ab4e59c0bb66016ea78b25bf
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2017-07-08 10:27:57 +0200

    Added rabbitmq and some simple controllers.

---
+++ src/Fibon.Api/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fibon.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET api/values
+        [HttpGet("")]
+        public IActionResult Get()
+        => Content("Hello from Fibon API");
+    }
+}
